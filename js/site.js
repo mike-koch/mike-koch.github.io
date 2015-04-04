306a759338377e04def07263ed8756a5ef823d8d
Mike Koch <mkoch227@gmail.com>
2015-04-03 21:12:47 -0400

    Set offset to scroll spy so everything lines up

--- js/site.js
+++ js/site.js
@@ -8,7 +8,8 @@ var loadJquery = function() {
 
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
-        target: '.navbar-fixed-top'
+        target: '.navbar-fixed-top',
+        offset: 70
     });
 
     // Closes the Responsive Menu on Menu Item Click
